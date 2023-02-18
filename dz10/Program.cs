35ba7584f0bc7b67edffa98b08a6ce231dad506d
Kondratyeva Tatyana <t-mo_39@outlook.com>
2023-02-18 23:13:30 +0300

    Выполнила дз

---
+++ dz10/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 10. Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+//456->5
+//782->8
+//918->1
+Console.WriteLine("Введите число: ");
+string SecondNumber = Console.ReadLine();
+int chars = Convert.ToInt32(SecondNumber);
+if(chars>99 && chars<1000)
+{
+    Console.WriteLine(SecondNumber.Substring(1,1));
+}
+else
+{
+    Console.WriteLine("Вы ввели неверное число!");
+}
---
+++ dz13/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+//645->5
+//78-> третьей цифры нет
+//32679->6
+Console.WriteLine("Введите число: ");
+string SecondNumber = Console.ReadLine();
+int chars = Convert.ToInt32(SecondNumber);
+if(chars>99)
+{
+    Console.WriteLine(SecondNumber.Substring(2,1));
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
